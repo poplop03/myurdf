358111b53a501a8cdae058dbefe5d3e259dbb06f
poplop03 <thienductang@gmail.com>
2025-04-28 16:35:55 +0700

    1st comm

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/duc/ROS/myurdf/src
+# Build directory: /home/duc/ROS/myurdf/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("assem1_v3")
---
+++ build/assem1_v2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/duc/ROS/myurdf/src/assem1_v2
+# Build directory: /home/duc/ROS/myurdf/build/assem1_v2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
